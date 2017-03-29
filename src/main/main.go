c4a4a4cbc58b58132a2b58b9fdec43a1bbf48b7e
vcollado <necseron@gmail.com>
2017-03-29 23:56:07 +0200

    added addTask function

--- src/main/main.go
+++ src/main/main.go
@@ -1,6 +1,36 @@
 package main
 
 import (
-
+	"fmt"
 )
 
+type task struct {
+	name        string
+	description string
+	isDone      bool
+	// todo... add field issuedAt
+}
+
+var tasksLimitCount int = 10
+var tasksCount int
+
+func addTask(tasks *chan task, name string, description string, isDone bool) {
+	tasksCount++
+	*tasks <- task{name: name, description: description, isDone: isDone}
+
+	if tasksCount == tasksLimitCount {
+		// close(tasks)
+	}
+}
+
+func main() {
+
+	tasks := make(chan task, tasksLimitCount)
+
+	addTask(&tasks, "this is the name of the task", "this is the description of the task", false)
+
+	for task := range tasks {
+		fmt.Println(task)
+	}
+
+}
