5b191c329c5221b3d084bc490daf48dabfe74ca4
vcollado <necseron@gmail.com>
2017-03-30 00:41:44 +0200

    added task add watcher with log

--- src/main/main.go
+++ src/main/main.go
@@ -2,29 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 type task struct {
 	name        string
 	description string
 	isDone      bool
-	// todo... add field issuedAt
 }
 
-func addTask(tasks *chan task, name string, description string, isDone bool) {
-	*tasks <- task{name, description, isDone}
+func addTask(tasks *chan *task, name string, description string, isDone bool) {
+	fmt.Println("adding the task, please wait...")
+	*tasks <- &task{name, description, isDone}
 }
 
 func main() {
 
-	tasks := make(chan task, 2)
+	tasks := make(chan *task)
 
-	addTask(&tasks, "first task", "this is the description of the first task", false)
-	addTask(&tasks, "second task", "today we are programming in go", false)
-	close(tasks)
+	go addTask(&tasks, "first task", "this is the description of the first task", false)
+	go addTask(&tasks, "second task", "today we are programming in go", false)
 
-	for task := range tasks {
-		fmt.Println(task)
+	for i := 0; i <= 1; i++ {
+		select {
+		case task := <-tasks:
+			log.Print("task added: ", task)
+		}
 	}
-
 }
