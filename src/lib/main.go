d839fc20972949198214875f8219672910737ef2
vcollado <necseron@gmail.com>
2017-03-31 00:31:22 +0200

    added the listeners for the added tasks, fully working in parallel

--- src/lib/main.go
+++ src/lib/main.go
@@ -6,10 +6,12 @@ type Task struct {
 	Done        bool
 }
 
+// GetName returns the name of the task
 func (t Task) GetName() string {
 	return t.Name
 }
 
+// IsDone check if the tasks is done
 func (t Task) IsDone() bool {
 	return t.Done
 }
--- src/main/main.go
+++ src/main/main.go
@@ -3,29 +3,49 @@ package main
 import (
 	"fmt"
 	"lib"
-	"log"
+	"time"
 )
 
-func addTask(tasks *chan *lib.Task, name string, description string, done bool) {
-	fmt.Println("adding the task, please wait...")
-	*tasks <- &lib.Task{Name: name, Description: description, Done: done}
+func taskListener(listenerId int, tasksToListenOver <-chan lib.Task, tasksEvents chan<- lib.Task) {
+	for task := range tasksToListenOver {
+		fmt.Println("listener", listenerId, "says: the following task has been added:", task)
+		// let the others listeners to read it
+		time.Sleep(time.Second * 1)
+		// respond to the event with the recently added task
+		tasksEvents <- task
+	}
+}
+
+func receiveTaskListener(tasksEventsToReceive chan lib.Task, tasksAddedCount int) {
+	// receive the values of the listener channel
+	for t := 0; t < tasksAddedCount; t++ {
+		<-tasksEventsToReceive
+	}
+}
+
+func addTask(tasks chan<- lib.Task, name string, description string, done bool) {
+	taskToAdd := lib.Task{Name: name, Description: description, Done: done}
+	tasks <- taskToAdd
 }
 
 func main() {
 
-	tasks := make(chan *lib.Task)
+	var maxTasksAllowed int = 3
+	tasks := make(chan lib.Task, maxTasksAllowed)
+	tasksEvents := make(chan lib.Task)
 
-	go addTask(&tasks, "first task", "this is the description of the first task", true)
-	go addTask(&tasks, "second task", "today we are programming in go", false)
+	// create 2 parallel foreground listeners that will read the tasks added
+	go taskListener(0, tasks, tasksEvents)
+	go taskListener(1, tasks, tasksEvents)
 
-	for i := 0; i <= 1; i++ {
-		select {
-		case task := <-tasks:
-			log.Print("task added: ", task)
-			if task.IsDone() {
-				log.Print("and its done")
-			}
-		}
-	}
+	addTask(tasks, "first task", "this is the description of the first task", true)
+	addTask(tasks, "second task", "today we are programming in go", false)
+	addTask(tasks, "last task", "some goroutines are made", true)
 
+	receiveTaskListener(tasksEvents, len(tasks))
 }
+
+// the ouput is the following:
+// listener 1 says: the following task has been added: {first task this is the description of the first task true}
+// listener 0 says: the following task has been added: {second task today we are programming in go false}
+// listener 1 says: the following task has been added: {last task some goroutines are made true}
